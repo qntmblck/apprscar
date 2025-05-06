a9390f195e301da5bce36c3b03cd2b78d823224a
qntmblck <jcomeaux@ug.uchile.cl>
2025-05-06 10:10:43 -0400

    master

--- resources/js/Components/Clients.jsx
+++ resources/js/Components/Clients.jsx
@@ -5,7 +5,7 @@ const clients = [
     { name: 'Walmart', logo: '/img/walmart.png?v=2', width: 140, height: 64, offsetX: 8, offsetY: 0 },
     { name: 'Deco Muebles', logo: '/img/dashboard/deco.png?v=2', width: 165, height: 60, offsetX: 22, offsetY: 2 },
     { name: 'Fruna', logo: '/img/dashboard/fruna.webp?v=2', width: 160, height: 66, offsetX: 2, offsetY: 2 },
-    { name: 'Fibox', logo: '/img/dashboard/fibox.png?v=2', width: 130, height: 60, offsetX: -15, offsetY: 1 },
+    { name: 'Fibox', logo: '/img/dashboard/fibox.png?v=2', width: 130, height: 60, offsetX: -15, offsetY: -2 },
     { name: 'Tottus', logo: '/img/tottus.png?v=2', width: 150, height: 60, offsetX: 4, offsetY: -1 },
     { name: 'Falabella', logo: '/img/dashboard/falabella.png?v=2', width: 140, height: 60, offsetX: 27, offsetY: -1 },
     { name: 'Prisa', logo: '/img/dashboard/prisa.png?v=2', width: 185, height: 75, offsetX: 2, offsetY: -2 },
@@ -13,7 +13,7 @@ const clients = [
     { name: 'Paris', logo: '/img/paris.png?v=2', width: 95, height: 40, offsetX: 15, offsetY: 16 },
     { name: 'Geoprospec', logo: '/img/dashboard/geo.png?v=2', width: 190, height: 70, offsetX: 35, offsetY: -4 },
     { name: 'Construmart', logo: '/img/contrumart.jpeg?v=2', width: 240, height: 83, offsetX: -15, offsetY: -14 },
-    { name: 'Tecnopapel', logo: '/img/tecnopapel.png?v=2', width: 180, height: 76, offsetX: -40, offsetY: -4 },
+    { name: 'Tecnopapel', logo: '/img/tecnopapel.png?v=2', width: 180, height: 76, offsetX: -43, offsetY: -4 },
   ]
 
   export default function Clients() {
