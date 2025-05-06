e6540cb6672b476e1fc461494793312f43248a30
qntmblck <jcomeaux@ug.uchile.cl>
2025-05-06 04:17:05 -0400

    master

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -17,11 +17,39 @@ export default function Welcome() {
         <Header />
         <main>
           <Hero />
+
+          {/* Separador más bajo y suave: Hero → Features */}
+          <svg className="w-full -mb-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+          </svg>
+
           <Features />
+
+          {/* Separador más bajo y suave: Features → Statistics */}
+          <svg className="w-full -mb-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#ffffff" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+          </svg>
+
           <Statistics />
+
+          {/* Separador más bajo y suave: Statistics → Clients */}
+          <svg className="w-full -mb-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+          </svg>
+
           <Clients />
+
+          {/* Separador plano entre Clients y CallToAction */}
+          <div className="h-4 bg-[#0c1e3a]" />
+
           <CallToAction />
         </main>
+
+        {/* Separador final: CTA → Footer */}
+        <svg className="w-full -mt-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#111827" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+        </svg>
+
         <Footer />
       </div>
     </>
