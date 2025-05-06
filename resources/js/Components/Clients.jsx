ea48a5d76b9d0241feb7675bb9a56f9c379cd041
qntmblck <jcomeaux@ug.uchile.cl>
2025-05-06 09:54:25 -0400

    master

--- resources/js/Components/Clients.jsx
+++ resources/js/Components/Clients.jsx
@@ -1,19 +1,19 @@
 const clients = [
-    { name: 'Latam', logo: '/img/latam.png?v=2' },
-    { name: 'Essity', logo: '/img/dashboard/essity.svg?v=2' },
-    { name: 'Ripley', logo: '/img/ripley.png?v=2' },
-    { name: 'Walmart', logo: '/img/walmart.png?v=2' },
-    { name: 'Deco Muebles', logo: '/img/dashboard/deco.png?v=2' },
-    { name: 'Fruna', logo: '/img/dashboard/fruna.webp?v=2' },
-    { name: 'Fibox', logo: '/img/dashboard/fibox.png?v=2' },
-    { name: 'Tottus', logo: '/img/tottus.png?v=2' },
-    { name: 'Falabella', logo: '/img/dashboard/falabella.png?v=2' },
-    { name: 'Prisa', logo: '/img/dashboard/prisa.png?v=2' },
-    { name: 'Canontex', logo: '/img/dashboard/canontex.png?v=2' },
-    { name: 'Paris', logo: '/img/paris.png?v=2' },
-    { name: 'Geoprospec', logo: '/img/dashboard/geo.png?v=2' },
-    { name: 'Construmart', logo: '/img/contrumart.jpeg?v=2' },
-    { name: 'Tecnopapel', logo: '/img/tecnopapel.png?v=2' },
+    { name: 'Latam', logo: '/img/latam.png?v=2', width: 140, height: 48, offsetX: -25, offsetY: 7 },
+    { name: 'Essity', logo: '/img/dashboard/essity.svg?v=2', width: 120, height: 50, offsetX: -15, offsetY: 4 },
+    { name: 'Ripley', logo: '/img/ripley.png?v=2', width: 150, height: 60, offsetX: -4, offsetY: -2 },
+    { name: 'Walmart', logo: '/img/walmart.png?v=2', width: 140, height: 64, offsetX: 8, offsetY: 0 },
+    { name: 'Deco Muebles', logo: '/img/dashboard/deco.png?v=2', width: 165, height: 60, offsetX: 22, offsetY: 2 },
+    { name: 'Fruna', logo: '/img/dashboard/fruna.webp?v=2', width: 160, height: 66, offsetX: 2, offsetY: 2 },
+    { name: 'Fibox', logo: '/img/dashboard/fibox.png?v=2', width: 130, height: 60, offsetX: -15, offsetY: 1 },
+    { name: 'Tottus', logo: '/img/tottus.png?v=2', width: 150, height: 60, offsetX: 4, offsetY: -1 },
+    { name: 'Falabella', logo: '/img/dashboard/falabella.png?v=2', width: 140, height: 60, offsetX: 27, offsetY: -1 },
+    { name: 'Prisa', logo: '/img/dashboard/prisa.png?v=2', width: 185, height: 75, offsetX: 0, offsetY: -2 },
+    { name: 'Canontex', logo: '/img/dashboard/canontex.png?v=2', width: 160, height: 70, offsetX: 0, offsetY: 2 },
+    { name: 'Paris', logo: '/img/paris.png?v=2', width: 95, height: 40, offsetX: 15, offsetY: 20 },
+    { name: 'Geoprospec', logo: '/img/dashboard/geo.png?v=2', width: 190, height: 70, offsetX: 35, offsetY: -4 },
+    { name: 'Construmart', logo: '/img/contrumart.jpeg?v=2', width: 240, height: 83, offsetX: -15, offsetY: -10 },
+    { name: 'Tecnopapel', logo: '/img/tecnopapel.png?v=2', width: 180, height: 76, offsetX: -40, offsetY: -4 },
   ]
 
   export default function Clients() {
@@ -25,17 +25,22 @@ const clients = [
               <div
                 key={`${client.name}-${index}`}
                 className="flex items-center justify-center flex-shrink-0 grayscale-[60%] opacity-90 hover:grayscale-0 hover:opacity-100 transition duration-300"
-                style={{ width: '160px', height: '64px' }}
+                style={{ width: `${client.width + 20}px`, height: `${client.height + 10}px` }}
               >
                 <img
-                  className="max-h-12 object-contain mx-auto"
                   src={client.logo}
                   alt={`Logo ${client.name}`}
+                  className="object-contain mx-auto"
+                  style={{
+                    width: `${client.width}px`,
+                    height: `${client.height}px`,
+                    transform: `translate(${client.offsetX}px, ${client.offsetY}px)`,
+                  }}
                 />
               </div>
             ))}
           </div>
         </div>
       </section>
-    );
+    )
   }
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -14,77 +14,74 @@ export default function Welcome() {
     <>
       <Head title="Transportes SCAR | Tu carga, nuestra misión" />
 
-      <div className="bg-white">
+      <div className="bg-white overflow-hidden">
         <Header />
-        <main>
+        <main className="overflow-hidden">
           <Hero />
 
           {/* Separador: Hero → Clients */}
-          <svg className="w-full -mb-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
-            <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
-          </svg>
+          <div className="relative -mt-1">
+            <svg className="w-full block leading-none" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+              <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+            </svg>
+          </div>
 
           <Clients />
 
           <Features />
 
           {/* Transición suave entre Features y Statistics */}
-          <div className="-mt-px h-6 w-full bg-gradient-to-b from-[#0c1e3a] to-[#f6f9ff]" />
+          <div className="relative -mt-px h-6 w-full bg-gradient-to-b from-[#0c1e3a] to-[#f6f9ff]" />
 
           <Statistics />
 
-          <svg className="w-full -mt-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
-  <path fill="#0c1e3a" d="M1440,0 C960,60 480,0 0,60 L0,0 L1440,0 Z" />
-  <rect x="0" y="0" width="1440" height="60" fill="#f6f9ff" mask="url(#curve)" />
-  <defs>
-    <mask id="curve">
-      <rect x="0" y="0" width="1440" height="60" fill="white" />
-      <path d="M1440,0 C960,60 480,0 0,60 L0,0 L1440,0 Z" fill="black" />
-    </mask>
-  </defs>
-</svg>
-
-
-
+          {/* Transición curva sin borde visible hacia Compromiso y Alianzas */}
+          <div className="relative -mt-1">
+            <svg className="w-full block leading-none" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+              <defs>
+                <mask id="curve">
+                  <rect x="0" y="0" width="1440" height="60" fill="white" />
+                  <path d="M1440,0 C960,60 480,0 0,60 L0,0 L1440,0 Z" fill="black" />
+                </mask>
+              </defs>
+              <rect x="0" y="0" width="1440" height="60" fill="#f6f9ff" mask="url(#curve)" />
+              <path d="M1440,0 C960,60 480,0 0,60 L0,0 L1440,0 Z" fill="#0c1e3a" />
+            </svg>
+          </div>
 
           {/* Nueva sección destacada de Compromiso y Alianzas */}
-
           <div className="bg-gray-50 py-6 px-4 sm:px-6 lg:px-6">
-
             <div className="mt-4">
               <Alliances />
             </div>
           </div>
 
-          <svg
-  className="w-full -mt-px block leading-none"
-  viewBox="0 0 1440 60"
-  xmlns="http://www.w3.org/2000/svg"
-  preserveAspectRatio="none"
->
-  <defs>
-    <mask id="bottom-curve">
-      <rect x="0" y="0" width="1440" height="60" fill="white" />
-      <path d="M0,60 C480,0 960,60 1440,0 L1440,60 L0,60 Z" fill="black" />
-    </mask>
-  </defs>
-  <rect x="0" y="0" width="1440" height="60" fill="#f6f9ff" mask="url(#bottom-curve)" />
-  <path d="M0,60 C480,0 960,60 1440,0 L1440,60 L0,60 Z" fill="#0c1e3a" />
-</svg>
-
+          {/* Transición curva sin borde visible hacia CallToAction */}
+          <div className="relative -mt-1">
+            <svg className="w-full block leading-none" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+              <defs>
+                <mask id="bottom-curve">
+                  <rect x="0" y="0" width="1440" height="60" fill="white" />
+                  <path d="M0,60 C480,0 960,60 1440,0 L1440,60 L0,60 Z" fill="black" />
+                </mask>
+              </defs>
+              <rect x="0" y="0" width="1440" height="60" fill="#f6f9ff" mask="url(#bottom-curve)" />
+              <path d="M0,60 C480,0 960,60 1440,0 L1440,60 L0,60 Z" fill="#0c1e3a" />
+            </svg>
+          </div>
 
           <CallToAction />
         </main>
 
         {/* Separador: CTA → Footer */}
-<svg className="w-full -mt-1" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
-  <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
-</svg>
-
+        <div className="relative -mt-1">
+          <svg className="w-full block" viewBox="0 0 1440 60" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#0c1e3a" d="M0,0 C480,60 960,0 1440,60 L1440,0 L0,0 Z" />
+          </svg>
+        </div>
 
         <Footer />
       </div>
     </>
   )
 }
-
